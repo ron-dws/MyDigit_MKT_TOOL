5bcc1351d1b8b59fc50729713c6b6f7c452a7e85
ron <wanko@digitwebsolution.com>
2020-05-31 22:07:58 -0500

    Updates

---
+++ precache-manifest.7a6d79ed54408576d5270950c3ac8e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd7d5a05d0a7698b8b5d9b64a8411c7",
+    "url": "/MyDigit_MKT_TOOL/index.html"
+  },
+  {
+    "revision": "19eb3582cf64ef23b733",
+    "url": "/MyDigit_MKT_TOOL/static/css/main.a7207554.chunk.css"
+  },
+  {
+    "revision": "b15339f05990eba3923f",
+    "url": "/MyDigit_MKT_TOOL/static/js/2.e7dcb3a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyDigit_MKT_TOOL/static/js/2.e7dcb3a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19eb3582cf64ef23b733",
+    "url": "/MyDigit_MKT_TOOL/static/js/main.6fe7c2d3.chunk.js"
+  },
+  {
+    "revision": "884498d32d3c030667c4",
+    "url": "/MyDigit_MKT_TOOL/static/js/runtime-main.0f585386.js"
+  }
+]);
