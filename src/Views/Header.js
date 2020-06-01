1d8b0d34c7589c4579932452e6e3d5ef384b6eee
ron <wanko@digitwebsolution.com>
2020-05-31 22:32:08 -0500

    img src adjusted

--- src/Views/Header.js
+++ src/Views/Header.js
@@ -5,7 +5,7 @@ function Header(props){
    const titre = "East Start Wireless";
     return(
         <div className="header-main">
-          <img src="/imgs/es_logo.png" alt="east star wireless" />
+          <img src="./imgs/es_logo.png" alt="east star wireless" />
           <h1>{ titre }</h1>
         </div>
     )
